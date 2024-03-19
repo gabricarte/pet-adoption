e1b73eb1de6b8ca1f55a13891ab1005b46df4b0a
Gabriela Goncalves Ricarte <ggricarte@rd.com.br>
2024-03-19 14:54:58 -0300

    populate owner object with data fetched from API

--- src/utils/ownerDataFetcher.js
+++ src/utils/ownerDataFetcher.js
@@ -1,14 +1,18 @@
+import { Owner } from "../models/Owner";
+
 export const fetchOwnerData = async () => {
-    try{
-        let ownerData = {};
+    try {
         const response = await fetch(`https://randomuser.me/api/`);
         const data = await response.json();
-        ownerData.name = data.results[0].name.first, data.results[0].name.last; 
-        ownerData.phone = data.results[0].phone;
-        ownerData.location = `${data.results[0].location.city}, ${data.results[0].location.country}`;
-        ownerData.picture = data.results[0].picture.medium;
+        const ownerData = new Owner(
+            `${data.results[0].name.first} ${data.results[0].name.last}`,
+            data.results[0].phone,
+            data.results[0].location.city,
+            data.results[0].location.country,
+            data.results[0].picture.medium
+        );
         return ownerData;
-    }catch(error){
+    } catch (error) {
         console.log(error);
-    };
-}
+    }
+};
